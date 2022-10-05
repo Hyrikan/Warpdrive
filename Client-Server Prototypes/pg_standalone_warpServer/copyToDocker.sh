1c1b751c2ec966328fec1da3914600ce93e46998
Joel Ziegler <cody14@freenet.de>
2022-10-05 09:01:48 +0200

    fixed copy scripts

--- Client-Server Prototypes/pg_standalone_warpServer/copyToDocker.sh
+++ Client-Server Prototypes/pg_standalone_warpServer/copyToDocker.sh
@@ -1,4 +1,4 @@
 # this script copies relevant files for the standalone warp server compilation to the docker directory of postgres
 #!/bin/bash
 
-cp CMakeLists.txt main.c pg_warpserver.h ../../MyDockers/postgres/warpServer
+cp CMakeLists.txt main.c pg_warpserver.h ../../Docker/postgres/warpServer
